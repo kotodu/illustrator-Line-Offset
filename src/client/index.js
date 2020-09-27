d759df98c8a5ddc3e8ec10a2eb7365706c98cd7c
kotodu <37821512+busroutemap@users.noreply.github.com>
2020-09-27 21:22:54 +0900

    illustratorからクライアント画面の移植

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,24 @@
 /* 1) Create an instance of CSInterface. */
-var csInterface = new CSInterface();
+var csi = new CSInterface();
 
 /* 2) Make a reference to your HTML button and add a click handler. */
-var openButton = document.querySelector("#open-button");
-openButton.addEventListener("click", openDoc);
+var openButton = document.querySelector("#option-submit");
+openButton.addEventListener("click", validOption);
 
 /* 3) Write a helper function to pass instructions to the ExtendScript side. */
-function openDoc() {
-  csInterface.evalScript("go()");
+function validOption() {
+    let frm = document.getElementById('ilo-options');
+    let fd = new FormData(frm);
+    const isSaved = csi.evalScript("isSaved()");
+    const isOpenDoc = csi.evalScript("isOpenDoc()");
+    if (!isOpenDoc) {
+        Window.alert("ドキュメントが開かれていません")
+        return
+    }
+    if (!isSaved) {
+        Window.alert("セーブしてください");
+    } else {
+        csi.evalScript(`generate(${fd.get("offsetCount")},${fd.get("offsetWidth")});`);
+    }
 }
+
